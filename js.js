af8091e0fbd6ca30d6409a8e839c989669b13f3d
William <william.teletabs@hotmail.com>
2022-03-18 19:32:36 -0300

    Criando 'for' para mostrar na tela imagens dos filmes selecionados

--- js.js
+++ js.js
@@ -1,5 +1,9 @@
-var listaFilmes = ["The Contact", "Um parto de viagem", "interestelar"];
+var listaFilmes = [
+  "https://paogeekeijo.com/wp-content/uploads/2020/05/contato-capa.jpg",
+  "http://3.bp.blogspot.com/-DQ4_ArK7WU0/TbglkdzqzSI/AAAAAAAABU8/5YKjuTtt6Y8/s1600/um-parto-de-viagem_2010.jpg",
+  "http://3.bp.blogspot.com/-tCBmjw7ti_M/VGDla6PAW_I/AAAAAAAAM8c/7vaEVkmio4g/s1600/Interestelar%2B01.jpg",
+];
 
-for (var i = 0; i < 3; i++) {
-  document.write("<p>" + listaFilmes[i] + "</p>");
+for (var i = 0; i < listaFilmes.length; i++) {
+  document.write("<img src=" + listaFilmes[i] + ">");
 }
